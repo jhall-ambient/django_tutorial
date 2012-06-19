61adacb14bed0097c8c900b777860d276408b532
Jason Hall <jhall@ambientdevices.com>
2012-06-19 15:15:33 -0400

    finished tut, tried on some Class based generic view stuff

--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,21 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView, ListView
+from polls.models import Poll
+from polls.views import PollDetailView
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('polls.views',
-    url(r'^$', 'index'),
-    url(r'^(?P<poll_id>\d+)/$', 'detail'),
-    url(r'^(?P<poll_id>\d+)/results/$', 'results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+urlpatterns = patterns('',
+    url(r'^$',
+        ListView.as_view(
+            queryset=Poll.objects.order_by('-pub_date')[:5],
+            context_object_name='latest_poll_list',
+            template_name='polls/index.html')),
+    url(r'^(?P<pk>\d+)/$',
+        PollDetailView.as_view()),
+    url(r'^(?P<pk>\d+)/results/$', 
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/results.html'),
+        name='poll_results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
 )
 
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,16 @@
-#from django.http import HttpResponse
-from polls.models import Poll
+from django.http import HttpResponse, HttpResponseRedirect
+from polls.models import Poll, Choice
+from django.core.urlresolvers import reverse
 #from django.template import Context, loader
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import Http404
+from django.template import RequestContext
+from django.views.generic import DetailView
+
+#- Only the Vote view is being used currently after switching to generic views
+#  (DetailView and ListView) that just take the default model objects and
+#  display them based on templates.  Had to also switch the reverse function to
+# the name we assigned for the results.
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -22,10 +30,35 @@ def detail(request, poll_id):
     #return HttpResponse("You're looking at poll %s" % poll_id)
     p = get_object_or_404(Poll, pk=poll_id)
     print "testing where this goes"
-    return render_to_response('polls/detail.html', {'poll': p})
+    return render_to_response('polls/detail.html', {'poll': p},
+                              context_instance=RequestContext(request))
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll': p})
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        print "No choice selected"
+        return render_to_response('polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after dealing with
+        # Post data successfully.  This prevents data from being posted twice
+        # if user hits back (basic rest stuff)
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
+    
+#  Testing subclassing Detail view for a view.
+class PollDetailView(DetailView):
+    print "Poll detail view: " 
+    template_name = "polls/detail.html"
+    model = Poll
+
+    print "template name set %s" % template_name
